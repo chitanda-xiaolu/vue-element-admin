721f94a7e80e65a50086d27af226bc27b848835d
chitanda-xiaolu <183138896@qq.com>
2022-05-13 00:19:41 +0800

    update

---
+++ src/layout/components/Sidebar/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="has-logo">
+
+  </div>
+</template>
+
+<script>
+// import { mapGetters } from 'vuex'
+// import Logo from './Logo'
+// import SidebarItem from './SidebarItem'
+// import variables from '@/styles/variables.scss'
+// export default {
+//   components: { SidebarItem, Logo }
+// }
+</script>
+
+<style>
+
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,15 +1,80 @@
 <template>
-  <div>
+  <div :class="calssObje" class="app-wrapper">
     layout
+    <div class="drawer-bg" @click="handleClickOutside"></div>
+    <sidebar class="sidebar-container" />
+    <div class="main-container">
+      <div class="fixed-header">
+        <navbar />
+        <tags-view />
+      </div>
+      <app-main />
+      <settings />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+// import RightPanel from '@components/RightPanel'
+import { AppMain, Navbar, Settings, Sidebar, TagsView } from './components'
 
+export default {
+  name: 'Layout',
+  components: {
+    AppMain,
+    Navbar,
+    Settings,
+    Sidebar,
+    TagsView
+  },
+  /**
+   * mixins是什么？
+   */
+  // mixins: [ResizeMixin],
+  computed: {
+    classObj() {
+      return null
+    }
+  },
+  methods: {
+    handleClickOutside() {
+      return null
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  @import "~@/styles/mixin.scss";
+  @import "~@/styles/variables.scss";
 
+  .app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+
+    &.mobile.openSidebar {
+      position: fixed;
+      top: 0;
+    }
+  }
+
+  .drawer-bg {
+    background: #000;
+    opacity: 0.3;
+    width: 100%;
+    top: 0;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+  }
+
+    .hideSidebar .fixed-header {
+    width: calc(100% - 54px)
+  }
+
+  .mobile .fixed-header {
+    width: 100%;
+  }
 </style>
